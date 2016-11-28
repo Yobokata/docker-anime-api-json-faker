66990466e6f0ef08ee70ed3658477dee9b82b5a3
E.G. ismaail <contact@ismaail.com>
2016-11-28 23:23:05 +0000

    first working app

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mhart/alpine-node
+MAINTAINER ismaail <contact@ismaail.com>
+
+# set environmental property for node modules
+ENV NODE_PATH /usr/lib/node_modules/
+
+# Create app directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package.json /usr/src/app/
+RUN npm install
+
+# Bundle app source
+COPY ./app/server.js /usr/src/app/server.js
+COPY ./app/public /usr/src/app/public
+
+EXPOSE 8200
+
+CMD ["npm", "start"]
---
+++ app/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const delay = require('express-delay');
+const app = express();
+const faker = require("faker");
+const _ = require("lodash");
+
+const port = 8200;
+
+// Allow CORS
+app.use(cors());
+
+// Static files directory
+app.use(express.static(__dirname + '/public'));
+
+// Dealy response
+app.use(delay(500));
+
+/**
+ * data contains list of Animes
+ */
+let data = _.times(14, function(n) {
+    let id = n + 1;
+    return {
+        id: id,
+        poster: `http://localhost:${port}/images/posters/${id}.jpg`,
+        title: faker.lorem.sentence(3),
+        videos: faker.random.number(300),
+        description: faker.lorem.sentences(8)
+    };
+});
+
+/**
+ * Find Single Anime by Id
+ * @param  {Number} id
+ * @return {Object}
+ */
+function find(id) {
+    return data.find(element => {
+        return parseInt(id) === element.id
+    });
+}
+
+/**
+ * Routes
+ */
+
+app.get('/anime', (req, res) => {
+    res.send(data);
+});
+
+app.get('/anime/:id', (req, res) => {
+    let params = req.params;
+    let obj = find(params.id);
+
+    if (! obj) {
+        res.status(404)
+           .send('Not found');
+        return
+    }
+
+    res.send(obj);
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`JSON Server is running on http://localhost:${port}`);
+});
