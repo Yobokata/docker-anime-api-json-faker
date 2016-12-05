0681fe003a34c755c3f2b1ef74b976fd485faf64
E.G. ismaail <contact@ismaail.com>
2016-12-05 00:51:43 +0000

    Indent fix

--- app/server.js
+++ app/server.js
@@ -25,14 +25,14 @@ app.use(delay(500));
  * data contains list of Animes
  */
 let data = _.times(14, function(n) {
-    let id = n + 1;
-    return {
-        id: id,
-        poster: `http://${host}/images/posters/${id}.jpg`,
-        title: faker.lorem.sentence(3),
-        videos: faker.random.number(300),
-        description: faker.lorem.sentences(8)
-    };
+  let id = n + 1;
+  return {
+    id: id,
+    poster: `http://${host}/images/posters/${id}.jpg`,
+    title: faker.lorem.sentence(3),
+    videos: faker.random.number(300),
+    description: faker.lorem.sentences(8)
+  };
 });
 
 /**
@@ -41,47 +41,50 @@ let data = _.times(14, function(n) {
  * @return {Object}
  */
 function find(id) {
-    return data.find(element => {
-        return parseInt(id) === element.id
-    });
-}
+  return data.find(element => {
+    return parseInt(id) === element.id
+  });
 
 /**
  * Routes
  */
 
+}
+// Animes list
 app.get('/anime', (req, res) => {
-    res.send(data);
+  res.send(data);
 });
 
+// Anime Details
 app.get('/anime/:id', (req, res) => {
-    let params = req.params;
-    let obj = find(params.id);
+  let params = req.params;
+  let obj = find(params.id);
 
-    if (! obj) {
-        res.status(404)
-           .send('Not found');
-        return
-    }
+  if (! obj) {
+    res.status(404)
+      .send('Not found');
+    return
+  }
 
-    res.send(obj);
+  res.send(obj);
 });
 
+// User Login
 app.post('/login', (req, res) => {
-    if ('doe' !== req.body.username || 'password' !== req.body.password) {
-        res.status(403)
-           .send('Wrong Credentials');
-        return;
-    }
+  if ('doe' !== req.body.username || 'password' !== req.body.password) {
+    res.status(403)
+      .send('Wrong Credentials');
+      return;
+  }
 
-    res.send({
-        username: "Jhon Doe",
-        accessToken: "Pr6UcSRzjIx5uHnodLvqdk8vPsikg="
-    });
+  res.send({
+    username: "Jhon Doe",
+    accessToken: "Pr6UcSRzjIx5uHnodLvqdk8vPsikg="
+  });
 });
 
 
 // Start the server
 app.listen(port, () => {
-    console.log(`JSON Server is running on http://localhost:${port}`);
+  console.log(`JSON Server is running on http://localhost:${port}`);
 });
